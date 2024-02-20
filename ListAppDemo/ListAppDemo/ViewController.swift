1397ba3f07cd56f1ddcaa5fe46111a83b0fe7f3b
Jaswitha-20 <jaswithakalikiri@gmail.com>
2024-02-20 11:34:05 -0600

    Jaswitha Kalikiri: completed the UI part

---
+++ ListAppDemo/ListAppDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ListAppDemo
+//
+//  Created by Kalikiri,Jaswitha on 2/20/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
